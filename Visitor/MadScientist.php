7fa0f720bd31b218b86eb765ac4fd791040a26a6
Trismegiste <florent.g@allopneus.com>
2013-06-02 21:38:47 +0200

    the true becomes false

--- Visitor/MadScientist.php
+++ Visitor/MadScientist.php
@@ -23,15 +23,19 @@ class MadScientist extends \PHPParser_NodeVisitorAbstract
     {
         switch ($node->getType()) {
 
+            case 'Scalar_FileConst':
+                return new \PHPParser_Node_Scalar_String($this->filename);
+                break;
+
             case 'Scalar_DirConst':
                 return new \PHPParser_Node_Scalar_String(dirname($this->filename));
                 break;
 
+            // mutation true => false
             case 'Expr_ConstFetch':
                 if ($node->name == 'true') {
                     return new \PHPParser_Node_Expr_ConstFetch(new \PHPParser_Node_Name('false'));
                 }
-
                 break;
         }
     }
